2dba370284f9b11414d2db8d3e1dee7d0aa5d893
William Min <wkmin1@uci.edu>
2023-02-01 14:40:39 -0800

    Add Dying Animation

--- Assets/Scripts/Animation/AnimationManager.cs
+++ Assets/Scripts/Animation/AnimationManager.cs
@@ -9,17 +9,28 @@ public class AnimationManager : MonoBehaviour
     [SerializeField]
     protected Animator m_Reference;
 
+    // Reference to Mesh Sprite
+    [SerializeField]
+    private GameObject sprite;
+
     [SerializeField]
     private ParameterRep<int> m_ActionState;
 
     [SerializeField]
     private ParameterRep<bool> m_IsMoving;
 
+    [SerializeField]
+    private ParameterRep<bool> m_IsDead;
+
     //Updates ResponseState to the animation's state
     void Update()
     {
         UpdateAction();
         UpdateMovement();
+        UpdateDeath();
+
+        if (sprite != null && !sprite.activeInHierarchy)
+            Destroy(m_Reference.gameObject);
     }
 
     private void UpdateAction()
@@ -36,5 +47,17 @@ public class AnimationManager : MonoBehaviour
             return;
 
         m_Reference.SetBool(m_IsMoving.ParameterName, m_IsMoving.ParameterValue);
+    }
+
+    private void UpdateDeath()
+    {
+        if (m_Reference == null)
+            return;
+
+        if (m_IsDead.ParameterValue)
+        {
+            m_Reference.SetTrigger(m_IsDead.ParameterName);
+            m_IsDead.ParameterValue = false;
+        }
     }
 }
