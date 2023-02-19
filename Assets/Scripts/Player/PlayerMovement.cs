2982300c69c2d87c613f0342dbf904de19f1dc8e
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2023-02-19 03:37:59 -0800

    Visualize the player velocity

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,19 +16,27 @@ public class PlayerMovement : MonoBehaviour
         startPosition = transform.position;
     }
 
+
+    // Get player's current speed
+    public float GetVelocity() {
+        return gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // update player position based on keyboard input
-        float translation = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
-        float rotation = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
-
-        transform.Translate(rotation,0,translation);
-
         // check if user has fallen the table, if so make them go back
         // to the origin
         if (transform.position.y < 0) {
             transform.position = startPosition;
         }
+    }
+
+    void FixedUpdate() {
+        // update player position based on keyboard input
+        float translation = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+
+        transform.Translate(rotation,0,translation);
     }
 }
---
+++ Assets/Scripts/Test/ShowPlayerSpeed.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPlayerSpeed : MonoBehaviour
+{
+    public Text playerSpeedText;
+    public PlayerMovement player;
+
+
+    void Update() {
+
+        playerSpeedText.text = player.GetVelocity().ToString();
+
+    }
+
+}
