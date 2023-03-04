f0a6de7bfbe4c233cb5ec32517ccb5e8e13769d8
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2023-03-04 00:55:21 -0800

    changed some layout
    add sprite to two players
    changed kitchen table layout

--- Assets/Scripts/Player/CameraSize.cs
+++ Assets/Scripts/Player/CameraSize.cs
@@ -7,30 +7,23 @@ public class CameraSize : MonoBehaviour
 {
 
     [SerializeField] private Camera cam;
-    [SerializeField] private PlayerSpawner playerSpawner;
-    [SerializeField] private int playerNumber = 1;
+    [SerializeField] private int playerId = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (playerNumber == 1) {
+        if (playerId == 1) {
             if (MainMenu.isMultiplayer) {
                 cam.rect = new Rect(0f, 0f, 0.5f, 1f);
             } else {
                 cam.rect = new Rect(0f, 0f, 1f, 1f);
             }
-        } else if (playerNumber == 2) {
+        } else if (playerId == 2) {
             if (MainMenu.isMultiplayer) {
                 cam.rect = new Rect(0.5f, 0f, 0.5f, 1f);
             } else {
                 cam.rect = new Rect(0f, 0f, 1f, 1f);
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
